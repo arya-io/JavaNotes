b45afb5d8dddf919eefd3ef807ece3cc92ce8e2f
Arya Bharne <127336304+arya-io@users.noreply.github.com>
2025-03-11 19:53:32 +0530

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -1,16 +1,15 @@
 
 public class Assignment7 {
 
-	public static void  main (String []args) {
-		int array[] = {50,-20,30,40,60,10};
-		if (array[0] == array[array.length-1]) {
-			System.out.println("first and last indexet ellement of array are same");
-			
+	public static void main(String[] args) {
+		int array[] = { 50, -20, 30, 40, 60, 10 };
+		if (array[0] == array[array.length - 1]) {
+			System.out.println("First and last indexed element of array are same.");
+
+		} else {
+			System.out.println("First and last indexed element of array are not same.");
+
 		}
-		else {
-			System.out.println("first and last indexet ellement of array are not same");
-			
-		}
-		}
-	
+	}
+
 }
